ba1c149217a3aa7acd0a8cac33c9aae64d6c1020
4the4ryushin <aman2001mi@gmail.com>
2022-06-03 23:24:52 +0530

    third commit, working with controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,17 +4,34 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
+  def test
+    render action: :new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/new'
-  get 'articles/create'
-  get 'articles/show'
-  get 'articles/update'
-  get 'articles/destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   get "/home", to: "home#home"
+  # resources :articles
+  get "/test", to: "articles#test"
+  get "/articles", to: "articles#index"
+  get "/articles/new", to: "articles#new"
+  get "articles/:id", to: "articles#show"
+  post "/articles", to: "articles#create"
 end
